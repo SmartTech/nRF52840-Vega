31bb93dc5a28f507a57542744b9915841c2984ab
Denis Silivanov <denis_n73@mail.ru>
2018-02-21 14:19:54 +0700

    123

--- IAR/settings/nRF52840.Debug.cspy.bat
+++ IAR/settings/nRF52840.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\Projects\Vega\nRF52840\IAR\settings\nRF52840.Debug.general.xcl" --backend -f "D:\Projects\Vega\nRF52840\IAR\settings\nRF52840.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\Projects\Vega\nRF52840-Vega\IAR\settings\nRF52840.Debug.general.xcl" --backend -f "D:\Projects\Vega\nRF52840-Vega\IAR\settings\nRF52840.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\Projects\Vega\nRF52840\IAR\settings\nRF52840.Debug.general.xcl" "--debug_file=%1" --backend -f "D:\Projects\Vega\nRF52840\IAR\settings\nRF52840.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\Projects\Vega\nRF52840-Vega\IAR\settings\nRF52840.Debug.general.xcl" "--debug_file=%1" --backend -f "D:\Projects\Vega\nRF52840-Vega\IAR\settings\nRF52840.Debug.driver.xcl" 
 
 @echo off 
 :end
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@ void uart_0_handler() {
   uint8_t byte;
   nRF52_UART_read (nRF52_UART0, &byte, 1);                // byte = Serial0.read();
   nRF52_UART_write(nRF52_UART1, &byte, 1);                // Serial1.write(byte);
-}                                                         
+}
                                                           
-void uart_1_handler() {                                   
+void uart_1_handler() {
   uint8_t byte;                                           
   nRF52_UART_read (nRF52_UART1, &byte, 1);                // byte = Serial1.read();
   nRF52_UART_write(nRF52_UART0, &byte, 1);                // Serial0.write(byte);
@@ -21,7 +21,7 @@ void setup() {
   
   nRF52_UART_init(nRF52_UART0, 115200, LINK_RX, LINK_TX); // Serial0.begin(115200);
   nRF52_UART_init(nRF52_UART1, 115200, P1_01,   P1_02);   // Serial1.begin(115200);
-    
+  
   nRF52_UART_attachCallback(nRF52_UART0, uart_0_handler); // Serial0.attachCallback(uart_0_handler);
   nRF52_UART_attachCallback(nRF52_UART1, uart_1_handler); // Serial1.attachCallback(uart_1_handler);
   
